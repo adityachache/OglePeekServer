c9fc671cfa75af0c8da348903bb6f118c41ca4d1
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-05-06 13:39:56 -0700

    Server listening and mongoDB set up

---
+++ models/User.js
@@ -0,0 +1,11 @@
+/*
+
+name
+email
+phoneNo
+productBought - [productIds]
+Addresss
+Bought History -> [Item1, Item2]
+
+Item1 would be like this -> ItemID, Address, Amount, Date, BillingNumber, Phone Number Used, Email Id Used, Coupons applied ?
+*/
