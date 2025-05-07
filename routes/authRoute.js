89eb1f500825b2fe5dc7798428f6406110537a38
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-05-07 16:34:05 -0700

    Added auth routes, need to configure the handlers and use Joi Schema for verification

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const { loginHandler, registerHandler } = require("../controllers/authController");
+const { catchAsync } = require("../utils/catchAsync")
+
+//Routee to handle login requests
+router.post("/login", verifyCustomerLogin, catchAsync(loginHandler));
+
+//Route to handle registration requests
+router.post("/register", verifyCustomerRegister, catchAsync(registerHandler));
+
 module.exports = router;
