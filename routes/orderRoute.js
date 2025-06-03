4c784edafff8851692ea1794e8f4ce9521dc1130
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-06-03 16:52:04 -0700

    Added orderRoute and order Controller

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { placeOrder } = require('../controllers/orderController');
+
+// POST /api/orders - Public (no auth required)
+router.post('/orders', placeOrder);
 
 module.exports = router;
